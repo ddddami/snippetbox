71cc99c88f12851a8562f8e23ff963eab6dc676c
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-04-07 23:16:18 +0100

    Show date in human readble format

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"snippetbox.damilola.dev/internal/models"
 )
@@ -24,8 +25,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 
 		name := filepath.Base(page)
-
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
@@ -42,4 +42,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("05 Dec 2006 at 15:00")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
