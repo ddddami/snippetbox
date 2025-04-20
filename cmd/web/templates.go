af37657f74ff637e832c480482e976823a62a8ac
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-04-20 13:59:33 +0100

    Embed templates and static files
    Embed templates and static files

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"snippetbox.damilola.dev/internal/models"
+	"snippetbox.damilola.dev/ui"
 )
 
 type templateData struct {
@@ -21,7 +23,7 @@ type templateData struct {
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
@@ -29,29 +31,32 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 
 		name := filepath.Base(page)
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
+			page,
+		}
+
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
 		cache[name] = ts
 	}
 	return cache, nil
 }
 
 func humanDate(t time.Time) string {
-	return t.Format("05 Dec 2006 at 15:00")
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
 	"humanDate": humanDate,
 }
+
