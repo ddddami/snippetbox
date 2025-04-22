f5d2a7a105d538cd734e3b202ea165d5e2f7fa16
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-04-22 12:20:26 +0100

    Test http handlers and middleware
    - ping/ and commonHeaders middleware

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,6 +10,10 @@ import (
 	"snippetbox.damilola.dev/internal/validator"
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
